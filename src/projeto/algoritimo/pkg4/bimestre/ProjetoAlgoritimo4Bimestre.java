4db0ad134fa597883a94f6c00d6fa1e7934abbf8
Aluno <Aluno@172.32.1.35>
2017-10-05 21:53:42 -0300

    criação do projeto no netbeans.

--- src/projeto/algoritimo/pkg4/bimestre/ProjetoAlgoritimo4Bimestre.java
+++ src/projeto/algoritimo/pkg4/bimestre/ProjetoAlgoritimo4Bimestre.java
@@ -3,10 +3,27 @@ package projeto.algoritimo.pkg4.bimestre;
 
 
 public class ProjetoAlgoritimo4Bimestre {
+public static void quadrado(double n){
+    double r = n*n;
+    System.out.println("");
+    
+}
+public static double metade(double v){
+    double r = v/2;
+    return r;
+    
+}
 
-   
     public static void main(String[] args) {
+       double raiz;
        
+       raiz = Math.sqrt(15);
+       
+        System.out.println("A raiz é" + raiz); 
+        
+        quadrado (15);
+        
+        System.out.println("A metade é:"+ metade(15));
     }
     
 }
